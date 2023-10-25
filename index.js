98d6975c25e814cce9442ad0eeb8d15e813e5fc6
raanausama <ruc619@gmail.com>
2023-10-25 23:35:48 +0500

    home apis done

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const authorize = require("./middleware/authorization.js");
 
 app.use(cors());
 
+app.use(express.static("./uploads"));
 
 const loginRouter = require("./routes/loginRoutes.js");
 const adminRouter = require("./routes/dashboard.js");
+
 const homeRouter = require("./routes/homeRoutes.js");
 
 //register and login routes
 app.use("/auth", loginRouter);
-app.use("/dashboard",adminRouter);
+app.use("/dashboard", adminRouter);
 
 //dashboard routes
 // app.use("/home", authorize , homeRouter);
 app.use("/home", homeRouter);
-
-
 
 app.listen(5000, () => {
   console.log("server is now running at 5000");
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
-const { Hero  } = require("../services/homeServices");
+const { sliderText, UploadImage, GetImage, heroSection, summerSection, winterSection, pastEvent, upcomingEvent, futureEvent  } = require("../services/homeServices");
 const router = Router();
 
 //register
-router.route("/insertText").post(Hero);
+router.route("/sliderText").post(sliderText);
+router.route("/upload-image").post(UploadImage);
+router.route("/get-images").get(GetImage);
+router.route("/heroSection").post(heroSection);
+router.route("/summerSection").post(summerSection);
+router.route("/winterSection").post(winterSection);
+router.route("/pastEvent").post(pastEvent);
+router.route("/upcomingEvent").post(upcomingEvent);
+router.route("/futureEvent").post(futureEvent);
 // router.route("/login").post(Login);
 // router.route("/isverify").get(isVerify);
 
